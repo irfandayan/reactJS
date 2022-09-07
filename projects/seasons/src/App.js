a64bb452f80ac8f8540f6b2a1c0aa3d3fa2928b4
irfan <engr.irfandayan@gmail.com>
2022-09-07 14:19:12 +0500

    Add state

--- projects/seasons/src/App.js
+++ projects/seasons/src/App.js
@@ -1,7 +1,21 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return <div> Hi there!</div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: 40 };
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
+  }
+
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
 }
 
 export default App;
