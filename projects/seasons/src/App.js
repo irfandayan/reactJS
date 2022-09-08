46b60092db7b6def6ee8dd17c6b7c857536f3086
irfan <engr.irfandayan@gmail.com>
2022-09-08 12:34:29 +0500

    Add class based state and display location

--- projects/seasons/src/App.js
+++ projects/seasons/src/App.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import './App.css';
+import Spinner from './Spinner';
+
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { lat: 40 };
+  state = { lat: null, errorMessage: '' };
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => console.log(err)
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <Spinner message="Please accept location request" />;
+  }
+
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
